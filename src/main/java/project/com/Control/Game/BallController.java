d154152fa7f2c502fd536d660cbbb9defd78ff94
CatarinaSousa115 <up202306419@edu.fe.up.pt>
2024-12-04 21:42:26 +0000

    Creation of the BallController class

---
+++ src/main/java/project/com/Control/Game/BallController.java
@@ -0,0 +1,11 @@
+package project.com.Control.Game;
+
+import project.com.Arkanoid;
+
+public class BallController extends GameController {
+
+    @Override
+    public void step(Arkanoid arkanoid) {
+
+    }
+}
