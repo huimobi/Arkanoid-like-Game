36ccd8033930d17de12520b34adfd5461aa5194c
CatarinaSousa115 <up202306419@edu.fe.up.pt>
2024-12-04 21:34:49 +0000

    Creation of the MenuController class

---
+++ src/main/java/project/com/Control/Menu/MenuController.java
@@ -0,0 +1,13 @@
+package project.com.Control.Menu;
+
+import project.com.Arkanoid;
+import project.com.Control.Controller;
+
+import java.awt.*;
+
+public abstract class MenuController<T extends Menu> extends Controller<T> {
+    @Override
+    public void step(Arkanoid arkanoid) {
+
+    }
+}
