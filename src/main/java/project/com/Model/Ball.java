04d1e44423edf6a4e25bcec1a0bd939d06a14ce2
joaomarques <joao.rodrigo.almeida.marques@gmail.com>
2024-12-04 19:15:46 +0000

    Fixed parameters

--- src/main/java/project/com/Model/Ball.java
+++ src/main/java/project/com/Model/Ball.java
@@ -1,8 +1,8 @@
 package project.com.Model;
 
 public class Ball extends Movable{
-    private int LENGTH=5;
-    private Ball(Position position){
+    private static final int LENGTH=5;
+    public Ball(Position position){
         super(position);
         super.setSize(LENGTH, LENGTH);
     }
--- src/main/java/project/com/Model/Movable.java
+++ src/main/java/project/com/Model/Movable.java
@@ -1,12 +1,17 @@
 package project.com.Model;
 
-import java.io.PipedOutputStream;
 import java.util.Vector;
 
 public abstract class Movable extends Element{
     private Vector<Position> positions;
-    private Position position;
     Movable(Position position){
         super(position);
+        this.positions=new Vector<>();
     };
+    public void addPosition(Position position){
+        this.positions.add(position);
+    }
+    public Vector<Position> getPositions(){
+        return positions;
+    }
 }
