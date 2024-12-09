c92199c23181b59e3d12c9067edd84be1311e107
huimobi <coelhopedro1905@gmail.com>
2024-12-09 00:45:32 +0000

    made viewerprovider a private class atribute and the get method

--- src/main/java/project/com/Viewer/Screen/Viewer.java
+++ src/main/java/project/com/Viewer/Screen/Viewer.java
@@ -1,14 +1,18 @@
 package project.com.Viewer.Screen;
 
+
+import project.com.Viewer.ViewerProvider;
 import project.com.gui.GUI;
 
 import java.io.IOException;
 
-    public abstract class Viewer<T> {
+public abstract class Viewer<T> {
         private final T model;
+        private final ViewerProvider viewerProvider;
 
-        public Viewer(T model) {
+        public Viewer(T model, ViewerProvider viewerProvider) {
             this.model = model;
+            this.viewerProvider=viewerProvider;
         }
 
         public T getModel() {
