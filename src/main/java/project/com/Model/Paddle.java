7484060c916018fc6d57b4a80ee79b4cff4fc2ae
huimobi <coelhopedro1905@gmail.com>
2024-12-09 00:52:54 +0000

    added a powerUp boolean to the model

--- src/main/java/project/com/Model/Paddle.java
+++ src/main/java/project/com/Model/Paddle.java
@@ -1,7 +1,12 @@
 package project.com.Model;
 
 public class Paddle extends Movable{
+    private boolean powerUp;
     Paddle(Position position) {
         super(position);
+        this.powerUp=false;
+    }
+    public Boolean getpowerUp(){
+        return powerUp;
     }
 }
