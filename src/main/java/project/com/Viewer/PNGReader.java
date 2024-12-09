f7c33b27cf40f8bb7fb669e6e3ff5c3b483b6e64
huimobi <coelhopedro1905@gmail.com>
2024-12-09 00:48:29 +0000

    made PNGReader give the ".png" in the end of the path. this way if another reader were implemented could do the same thing with the end changed.

--- src/main/java/project/com/Viewer/PNGReader.java
+++ src/main/java/project/com/Viewer/PNGReader.java
@@ -2,11 +2,8 @@ package project.com.Viewer;
 
 import com.googlecode.lanterna.TextColor;
 import project.com.Model.Position;
-import project.com.Viewer.ImageReader;
 import project.com.gui.GUI;
-
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
@@ -15,34 +12,17 @@ import java.util.Objects;
 
 public class PNGReader implements ImageReader {
     private final BufferedImage image;
-    private Position position;
 
     public PNGReader(String filepath) throws IOException {
-        URL resource = getClass().getClassLoader().getResource(filepath);
+        URL resource = getClass().getClassLoader().getResource(filepath + ".png");
         this.image= ImageIO.read(Objects.requireNonNull(resource));
     }
 
     //draws png
     public void draw(GUI gui, Position position){
-        this.position=position;
         gui.drawImage(image,position);
     }
 
-
-    public ArrayList<Position> getForeground() {
-        ArrayList<Position> white_pixels= new ArrayList<>();
-
-        for (int px = 0; px < image.getWidth(); px++) {
-            for (int py = 0; py < image.getHeight(); py++) {
-                int rgb=image.getRGB(px,py);
-                if(new Color(rgb,true).getAlpha()==0) continue;
-                if(Color.white.equals(new Color(rgb,true))){
-                    white_pixels.add(new Position(px+position.getX(),py+position.getY()));
-                }
-            }
-        }
-        return white_pixels;
-    }
 
     public void changePixelColor(GUI gui, ArrayList<Position> positions, TextColor color){
         for(Position p: positions){
@@ -53,5 +33,4 @@ public class PNGReader implements ImageReader {
     public BufferedImage getImage() {
         return image;
     }
-
 }
