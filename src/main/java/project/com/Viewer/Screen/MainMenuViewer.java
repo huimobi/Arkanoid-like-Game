b59a06beb37405ce6f9f379a782b95b34c2f1c2c
huimobi <coelhopedro1905@gmail.com>
2024-12-09 00:51:16 +0000

    added new atributes to the MainMenuViewer

--- src/main/java/project/com/Viewer/Screen/MainMenuViewer.java
+++ src/main/java/project/com/Viewer/Screen/MainMenuViewer.java
@@ -1,33 +1,43 @@
 package project.com.Viewer.Screen;
 
-/*public class MainMenuViewer<MainMenu> extends Viewer<> {
+import com.googlecode.lanterna.TextColor;
+import project.com.MainMenu;
+import project.com.Viewer.MainMenu.LogoViewer;
+import project.com.Viewer.MainMenu.MainMenuBackgroundViewer;
+import project.com.Viewer.ViewerProvider;
+import project.com.gui.GUI;
+
+import java.io.IOException;
+
+public class MainMenuViewer extends Viewer<MainMenu> {
 
         public static final TextColor unselectedColor = new TextColor.RGB(234,234,234);
         public static final TextColor selectedColor = new TextColor.RGB(99,155,255);
         public static final TextColor backgroundColor = new TextColor.RGB(28, 28, 46);
         public static final TextColor frameColor = new TextColor.RGB(255, 255, 255);
-        private final EntryViewer entryViewer;
-        private final LogoViewer logoViewer;
+        private final MainMenuBackgroundViewer background;
+        //private final EntryViewer entryViewer;
+        private final LogoViewer LogoViewer;
 
-        public MainMenuViewer(T model) {
-            super(model);
-            this.entryViewer = viewerProvider.getEntryViewer();
-            this.logoViewer = viewerProvider.getLogoViewer();
+        public MainMenuViewer(MainMenu model, ViewerProvider viewerProvider) {
+            super(model,viewerProvider);
+            //this.entryViewer = viewerProvider.getEntryViewer();
+            this.LogoViewer = viewerProvider.getLogoViewer();
+            this.background = viewerProvider.getMainMenuBackground();
         }
 
         @Override
         public void draw(GUI gui) throws IOException {
             gui.clear();
-            drawBackgroundAndFrame(gui, frameColor, backgroundColor);
-            drawEntries(gui, getModel().getEntries());
-            logoViewer.draw(gui, 44, 16);
+            //drawBackgroundAndFrame(gui, frameColor, backgroundColor);
+            //drawEntries(gui, getModel().getEntries());
+            //logoViewer.draw(gui, 44, 16);
             gui.refresh();
         }
 
-        private void drawEntries(ResizableGUI gui, List<Entry> entries) {
+        /*private void drawEntries(GUI gui, List<Entry> entries) {
             for (Entry entry: entries)
                 entryViewer.draw(entry, gui, getModel().getCurrentEntry() == entry ? selectedColor : unselectedColor);
-        }
-    }
+        }*/
 
-}*/
+}
