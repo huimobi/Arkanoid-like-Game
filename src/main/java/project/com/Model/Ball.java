9b4cadf2ab0205621e6c3afbf883104e1f52cb73
huimobi <coelhopedro1905@gmail.com>
2024-12-11 17:54:36 +0000

    minor changes

--- src/main/java/project/com/Model/Ball.java
+++ src/main/java/project/com/Model/Ball.java
@@ -2,7 +2,7 @@ package project.com.Model;
 
 public class Ball extends Movable{
     private int LENGTH=5;
-    private Ball(Position position){
+    Ball(Position position){
         super(position);
         super.setSize(LENGTH, LENGTH);
     }
