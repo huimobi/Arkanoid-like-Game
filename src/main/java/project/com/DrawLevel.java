d4803fab1fc0213623b1a60e2f93ccb6bef86af7
huimobi <coelhopedro1905@gmail.com>
2024-12-11 14:51:04 +0000

    writetext is no longer needed. changed it on MainMenu and drawLevel

--- src/main/java/project/com/DrawLevel.java
+++ src/main/java/project/com/DrawLevel.java
@@ -4,7 +4,7 @@ package project.com;
 import project.com.Model.Position;
 import project.com.Viewer.PNGReader;
 import project.com.Viewer.ViewerProvider;
-import project.com.Viewer.WriteText;
+
 import project.com.gui.GUI;
 
 import java.io.IOException;
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 
 public class DrawLevel {
-    private final GUI gui;
+    /*private final GUI gui;
 
     DrawLevel(GUI gui) throws IOException {
         this.gui=gui;
@@ -24,15 +24,15 @@ public class DrawLevel {
 
 
         //draw background
-        PNGReader gameBackground= new PNGReader("Game/background");
+        PNGReader gameBackground= new PNGReader("Game/background.png");
         gameBackground.draw(gui,new Position(0,0));
 
         //draw ball
-        PNGReader ball = new PNGReader("Elements/Ball/ball");
+        PNGReader ball = new PNGReader("Elements/Ball/ball.png");
         ball.draw(gui,new Position(80, 90));
 
         //draw paddle
-        PNGReader paddle = new PNGReader("Elements/Paddle/paddle");
+        PNGReader paddle = new PNGReader("Elements/Paddle/paddle.png");
         paddle.draw(gui,new Position(60, 130));
 
         //draw all blocks and the top wall
@@ -41,13 +41,13 @@ public class DrawLevel {
         for (int j = 0; j < 9; j++) {
             for (int i = 0; i <= 10; i++) {
                 if(j==0){
-                    PNGReader brick = new PNGReader("Elements/Brick/unbreakableBrick");
+                    PNGReader brick = new PNGReader("Elements/Brick/unbreakableBrick.png");
                     brick.draw(gui,block_p);
                     block_p.setX(block_p.getX() + 15);
                     continue;
                 }
                 if(blockSelector>4) blockSelector-=4;
-                PNGReader brick = new PNGReader("Elements/Brick/brick" +String.valueOf(blockSelector));
+                PNGReader brick = new PNGReader("Elements/Brick/brick" +String.valueOf(blockSelector)+".png");
                 brick.draw(gui,block_p);
                 block_p.setX(block_p.getX() + 15);
                 blockSelector++;
@@ -69,7 +69,7 @@ public class DrawLevel {
         scoreNumber.drawText(gui,numberScorePosition);
 
         //Lives display
-        PNGReader lives= new PNGReader("Game/lives");
+        PNGReader lives= new PNGReader("Game/lives.png");
         Position livesPos= new Position(gui.getWidth()-3*14-3,3);
         for(int i=0;i<3;i++){
             lives.draw(gui,livesPos);
@@ -93,5 +93,5 @@ public class DrawLevel {
         Highscore_value.drawText(gui,new Position((((gui.getWidth()-(Highscore_value.getLength()*7)+180)/2)),gui.getHeight()/2+14));
 
         gui.refresh();
-    }
+    }*/
 }
--- src/main/java/project/com/MainMenu.java
+++ src/main/java/project/com/MainMenu.java
@@ -1,30 +1,33 @@
 package project.com;
 
 import com.googlecode.lanterna.input.KeyStroke;
+import project.com.Model.Option;
 import project.com.Model.Position;
 import project.com.Viewer.PNGReader;
-import project.com.Viewer.WriteText;
+import project.com.Viewer.WriteChar;
+
 import project.com.gui.GUI;
 
 import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
-public class MainMenu extends Menu {
-
-    private WriteText cur_selection; //PLAY is the option by default
-    private boolean in_menu=true;
-    private final ArrayList<WriteText> options=new ArrayList<>();
+public class MainMenu{
+    /*private WriteText cur_selection; //PLAY is the option by default
+    private boolean in_menu = true;
+    private final ArrayList<WriteText> options = new ArrayList<>();
     private final GUI gui;
 
     public MainMenu(GUI gui) throws IOException, NullPointerException {
-        this.gui=gui;
+        this.gui = gui;
         options.add(new WriteText("Play"));
         options.add(new WriteText("Settings"));
         options.add(new WriteText("Info"));
         options.add(new WriteText("exit"));
-        this.cur_selection=options.getFirst(); //first option as default
+        this.cur_selection = options.getFirst(); //first option as default
         draw();
         run();
     }
@@ -39,72 +42,115 @@ public class MainMenu extends Menu {
     //draws the Main Menu
     private void draw() throws IOException {
         gui.clear();
-        PNGReader background_image= new PNGReader("Menu/background");
-        background_image.draw(this.gui,new Position(0,0));
+        PNGReader background_image = new PNGReader("Menu/background");
+        background_image.draw(this.gui, new Position(0, 0));
 
 
         //print "play"
-        Position p_position= new Position((gui.getWidth()-4*7)/2,(gui.getHeight()+7)/2);
-        for(WriteText option: options){
-            option.drawText(this.gui,new Position((gui.getWidth()-option.getLength()*7)/2, p_position.getY()));
-            p_position.setY(p_position.getY()+18);
+        Position p_position = new Position((gui.getWidth() - 4 * 7) / 2, (gui.getHeight() + 7) / 2);
+        for (WriteText option : options) {
+            option.drawText(this.gui, new Position((gui.getWidth() - option.getLength() * 7) / 2, p_position.getY()));
+            p_position.setY(p_position.getY() + 18);
 
         }
 
         //Arkanoid LOGO
-        PNGReader arkanoid=new PNGReader("Menu/arkanoid");
-        arkanoid.draw(this.gui,new Position(70,36));
+        PNGReader arkanoid = new PNGReader("Menu/arkanoid");
+        arkanoid.draw(this.gui, new Position(70, 36));
 
-        options.getFirst().setForegroundColor(gui,"#ff7129"); //shows first option selected as default
+        options.getFirst().setForegroundColor(gui, "#ff7129"); //shows first option selected as default
         this.gui.refresh();
     }
-
 
 
     private void processKey(KeyStroke key) throws IOException {
         switch (key.getKeyType()) {
             case ArrowUp -> previous_option();
             case ArrowDown -> next_option();
-            case Enter -> {select() ;in_menu=false;}
-            case EOF -> {gui.close(); in_menu=false;}
+            case Enter -> {
+                select();
+                in_menu = false;
             }
+            case EOF -> {
+                gui.close();
+                in_menu = false;
+            }
+        }
 
     }
 
     //previous_option
     void previous_option() throws IOException {
-        int index=options.indexOf(cur_selection);
-        if(index==0) return;//checks if the cur_option is the first in the menu
+        int index = options.indexOf(cur_selection);
+        if (index == 0) return;//checks if the cur_option is the first in the menu
 
         options.get(index).setForegroundDefault(gui);
         gui.refresh();
-        options.get(index-1).setForegroundColor(gui,"#ff7129");
-        cur_selection=options.get(index-1);
+        options.get(index - 1).setForegroundColor(gui, "#ff7129");
+        cur_selection = options.get(index - 1);
         gui.refresh();
 
     }
 
     //next_option
     void next_option() throws IOException {
-        int index=options.indexOf(cur_selection);
-        if(index==options.size()-1) return; // checks if the cur_option is the last in the menu
+        int index = options.indexOf(cur_selection);
+        if (index == options.size() - 1) return; // checks if the cur_option is the last in the menu
 
         options.get(index).setForegroundDefault(gui);
         gui.refresh();
-        options.get(index+1).setForegroundColor(gui,"#ff7129");
-        cur_selection=options.get(index+1);
+        options.get(index + 1).setForegroundColor(gui, "#ff7129");
+        cur_selection = options.get(index + 1);
         gui.refresh();
 
     }
 
     //select option
     private void select() throws IOException {
-        switch (cur_selection.getString()){
-            case "PLAY"-> {
-                new DrawLevel(gui);}//Game();
-            case "SETTINGS"-> System.out.print("SETTINGS"); // Settings();
+        switch (cur_selection.getString()) {
+            case "PLAY" -> {
+                new DrawLevel(gui);
+            }//Game();
+            case "SETTINGS" -> System.out.print("SETTINGS"); // Settings();
             case "INFO" -> System.out.print("INFO"); //Info();
-            case "EXIT"-> gui.close();
+            case "EXIT" -> gui.close();
         }
+    }*/
+    private final ArrayList<Option> options;
+    private int currentOption = 0;
+
+    public MainMenu() {
+       this.options= createOptions();
     }
+
+
+    public ArrayList<Option> getOptions() {
+        return options;
+    }
+
+    public int getNumberEntries() {
+        return this.options.size();
+    }
+
+    public void moveDown() {
+        currentOption++;
+        currentOption = currentOption % getNumberEntries();
+    }
+
+    public void moveUp() {
+        currentOption += getNumberEntries() - 1;
+        currentOption = currentOption % getNumberEntries();
+    }
+
+    public Option getCurrentOption() {
+        return options.get(currentOption);
+    }
+
+    private ArrayList<Option> createOptions() {
+        ArrayList<Option> options=new ArrayList<>();
+        options.add(new Option(new Position((Arkanoid.WIDTH- 5 * 7) / 2+1, (Arkanoid.HEIGHT + 7) / 2), Option.Type.START_GAME));
+        options.add( new Option(new Position((Arkanoid.WIDTH- 4 * 7) / 2, ((Arkanoid.HEIGHT + 7) / 2 )+18), Option.Type.INFO));
+        options.add(new Option(new Position((Arkanoid.WIDTH- 4 * 7) / 2, ((Arkanoid.HEIGHT + 7) / 2 )+36), Option.Type.EXIT));
+        return options;
+}
 }
