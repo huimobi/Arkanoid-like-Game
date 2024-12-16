9926ec9e0850257598883f10eef0090862182eda
CatarinaSousa115 <up202306419@edu.fe.up.pt>
2024-12-16 15:35:12 +0000

    add comments to the OptionsControllerTest

--- src/test/java/Control/Menu/OptionsControllerTest.java
+++ src/test/java/Control/Menu/OptionsControllerTest.java
@@ -57,41 +57,45 @@ public class OptionsControllerTest {
 
     @Test
     public void testStepInfo() throws IOException, URISyntaxException, FontFormatException {
+        // Configurar a opção atual do menu como INFO
         Option infoOption = new Option(new Position(0, 1), Option.Type.INFO);
         when(menu.getCurrentOption()).thenReturn(infoOption);
-
+        // Simular a ação SELECT
         controller.step(arkanoid, GUI.ACTION.SELECT, 0);
-
+        // Verificar se o estado foi mudado para InfoMenuState
         verify(arkanoid).setState(any(InfoMenuState.class));
     }
 
     @Test
     public void testStepExit() throws IOException, URISyntaxException, FontFormatException {
+        // Configurar a opção atual do menu como EXIT
         Option exitOption = new Option(new Position(0, 2), Option.Type.EXIT);
         when(menu.getCurrentOption()).thenReturn(exitOption);
-
+        // Simular a ação SELECT
         controller.step(arkanoid, GUI.ACTION.SELECT, 0);
-
+        // Verificar se o estado foi definido como null
         verify(arkanoid).setState(null);
     }
 
     @Test
     public void testStepToMainMenu() throws IOException, URISyntaxException, FontFormatException {
+        // Configurar a opção atual do menu como TO_MAIN_MENU
         Option toMainMenuOption = new Option(new Position(0, 3), Option.Type.TO_MAIN_MENU);
         when(menu.getCurrentOption()).thenReturn(toMainMenuOption);
-
+        // Simular a ação SELECT
         controller.step(arkanoid, GUI.ACTION.SELECT, 0);
-
+        // Verificar se o estado foi mudado para MainMenuState
         verify(arkanoid).setState(any(MainMenuState.class));
     }
 
     @Test
     public void testStepNoAction() throws IOException, URISyntaxException, FontFormatException {
+        // Configurar uma opção válida de "Start Game" com a posição (0, 0)
         Option startGameOption = new Option(new Position(0, 0), Option.Type.START_GAME);
         when(menu.getCurrentOption()).thenReturn(startGameOption);
-
+        // Simular uma ação diferente de SELECT
         controller.step(arkanoid, GUI.ACTION.NONE, 0);
-
+        // Verificar que o estado não foi alterado
         verify(arkanoid, never()).setState(any());
     }
 
